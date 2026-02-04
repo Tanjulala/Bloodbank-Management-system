0d5909b5f2c64c0cff4c47cc7ec832bc8f205a40
Asiful Islam <asifulislam7@gmail.com>
2026-02-04 17:50:20 +0600

    added main file

---
+++ Main.java
@@ -0,0 +1,15 @@
+import emManage.*;
+import entity.*;
+import gui.*;
+import reg.*;
+import IInter.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cover ob = new Cover();
+        ob.setVisible(true);
+
+    }
+}
